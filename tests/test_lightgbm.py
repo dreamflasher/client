5d28c6c9a4cbf17b436af95fcb18d98acc67a77b
Lavanya Shukla <lavanyashukla@gmail.com>
2019-12-16 17:27:39 -0800

    LightGBM support! ^_^

---
+++ tests/test_lightgbm.py
@@ -0,0 +1,24 @@
+import pytest
+import numpy as np
+import lightgbm as lgb
+import wandb
+import sys
+from wandb import wandb_run
+from wandb.lightgbm import wandb_callback
+
+# Tests which rely on row history in memory should set `History.keep_rows = True`
+from wandb.history import History
+History.keep_rows = True
+
+
+@pytest.fixture
+def dummy_data(request):
+    data = np.array([[1, 2], [3, 4]])
+    label = np.array([0, 1])
+    dtrain = lgb.Dataset(data, label=label)
+    return dtrain
+
+def test_basic_lightgbm(dummy_data, wandb_init_run):
+    param = {'max_depth': 2, 'eta': 1}
+    num_round = 2
+    lgb.train(param, dummy_data, num_round, callbacks=[wandb_callback()])
